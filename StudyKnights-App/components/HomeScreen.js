50ebee2a38278b7a063b56c0f10c91e5bc6e3a73
Hannah Moss <46806107+mosshan@users.noreply.github.com>
2020-11-04 00:48:05 -0500

    Update HomeScreenjs

--- StudyKnights-App/components/HomeScreen.js
+++ StudyKnights-App/components/HomeScreen.js
@@ -1,25 +1,97 @@
 // Homescreen.js
 import React, { Component } from 'react';
-import { Button, View, Text } from 'react-native';
-import { createStackNavigator, createAppContainer } from 'react-navigation';
+import {View, StyleSheet, Text , Dimensions, Image} from 'react-native';
+import imageSource from '../assets/map.png';
+import MapView, {Callout, Marker}  from "react-native-maps";
 
 export default class Home extends Component {
+
+  state = {
+    showRating: false,
+    markers: [
+      {
+        coordinate: {
+          latitude: 28.601957,
+          longitude: -81.200429,
+        },
+        title: "Student Union",
+      },
+      {
+        coordinate: {
+          latitude: 28.601514,
+          longitude: -81.198803,
+        },
+        title: "Engineering 1",
+      },
+    ],
+    region: {
+      latitude: 28.602560,
+      longitude: -81.200080,
+      latitudeDelta: 0.007,
+      longitudeDelta: 0.007,
+    },
+  };
+
+  toggleStatus(marker){
+    this.setState({
+      showRating:true
+    });
+    alert('toggle button handler: '+ this.state.showRating);
+  };
+
   render() {
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Home Screen</Text>
-        
-        <Button
-        title="Login"
-        onPress={() => this.props.navigation.navigate('Login')}
-        />
-
-        <Button
-        title="Register"
-        onPress={() => this.props.navigation.navigate('SignUp')}
-        />
-
-      </View>
-    )
+        <MapView
+          ref={map => this.map = map}
+          initialRegion={this.state.region}
+          style={styles.mapStyle}
+          mapType='satellite'
+        >
+          {this.state.markers.map((marker, index) => {
+            return (
+                 <Marker key={index} coordinate={marker.coordinate} title={marker.title}
+                  description={marker.description} onPress={() => {this.toggleStatus()}} >
+                  <Callout
+                      tooltip={true}
+                    >
+                      <Text style={styles.description} >
+                        {marker.title}
+                      </Text>
+                  </Callout>
+              </Marker>
+            );
+          })}
+          {this.state.showRating &&
+          <Text style={styles.description} >
+              lol
+          </Text>
+          }
+        </MapView>
+    );
   }
-}
+};
+
+const styles = StyleSheet.create({
+  mapStyle: {
+    flex: 1,
+    flexDirection: 'column-reverse',
+    height: Dimensions.get('window').height,
+    width: Dimensions.get('window').width,
+  },
+  description: {
+    //margin: 15,
+    fontSize: 15,
+    fontFamily: 'monospace',
+    padding: 5,
+    color: 'white',
+    backgroundColor: 'rgba(52, 52, 52, 1.0)'
+  },
+  rating: {
+    //margin: 15,
+    fontSize: 15,
+    fontFamily: 'monospace',
+    padding: 5,
+    color: 'white',
+    backgroundColor: 'rgba(52, 52, 52, 1.0)'
+  },
+});
