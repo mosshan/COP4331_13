f45f8b9a3165200f0ee9adee401562e98d76f1a2
Hannah Moss <46806107+mosshan@users.noreply.github.com>
2020-11-09 19:36:07 -0500

    Added StudySpots.js
    - plan to create scroll view with spots for map markers

---
+++ StudyKnights-App/frontend/src/components/StudySpots.js
@@ -0,0 +1,14 @@
+// Mapscreen.js	
+import React, {Component} from 'react';
+import {View, Text} from 'react-native';
+//import { Rating } from 'react-native-elements';
+
+export default class Spot extends Component {
+    render() {
+        return (
+          <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <Text>Login Screen</Text>
+          </View>
+        )
+      }
+}
