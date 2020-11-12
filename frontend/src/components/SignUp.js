c77424a7762fdb7728d189f06ba5335513678cab
MorandR <70453767+MorandR@users.noreply.github.com>
2020-11-11 19:12:52 -0500

    Update SignUp.js

--- frontend/src/components/SignUp.js
+++ frontend/src/components/SignUp.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import {Link} from 'react-router-dom';
-import './SignUp.css';
+import './CSS/SignUp.css';
 // import {Button} from './Button';
 
 function SignUp() {
