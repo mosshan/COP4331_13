be4ad2edf88efebffc8d188bcbd9437d6a4812ca
Dominic Simon <dominicsimon98@gmail.com>
2020-11-20 19:10:35 -0500

    Minor Syntax Change

--- StudyKnights-App/frontend/src/pages/ForgotPassword.js
+++ StudyKnights-App/frontend/src/pages/ForgotPassword.js
@@ -49,7 +49,7 @@ export default class ForgotPassword extends Component
          else
          {
            alert('A password reset email has been sent.');
-           this.props.navigation.navigate('LoginScreen');
+           this.props.navigation.navigate('Login');
          }
 
       })
