b4224e768c5acb9c48cbecec46230bdb662b8132
Hannah Moss <46806107+mosshan@users.noreply.github.com>
2020-11-22 03:47:03 -0500

    Update App.js

--- StudyKnights-App/frontend/App.js
+++ StudyKnights-App/frontend/App.js
@@ -1,45 +1,9 @@
 
 // App.js
 import React from 'react';
-import { StyleSheet} from 'react-native';
-import { createStackNavigator} from '@react-navigation/stack';
-import { NavigationContainer } from '@react-navigation/native';
+import MainNavigation from './src/pages/MainNavigation'
 
-import SignUpScreen from './src/pages/SignUpScreen';
-import LoginScreen from './src/pages/LoginScreen';
-import MapScreen from './src/pages/MapScreen';
-import SpotScreen from './src/pages/SpotScreen';
-import ForgotPassword from './src/pages/ForgotPassword
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <NavigationContainer>
-        <MyStack />
-      </NavigationContainer>
-    );
-  }
+export default function App (){
+    return  <MainNavigation />
 } 
-
-const Stack = createStackNavigator();
-
-function MyStack() {
-  return (
-    <Stack.Navigator initialRouteName="Map">
-      <Stack.Screen name="Map" component={MapScreen} />
-      <Stack.Screen name="SignUp" component={SignUpScreen} />
-      <Stack.Screen name="Login" component={LoginScreen} />
-      <Stack.Screen name="SpotPage" component={SpotScreen} />
-      <Stack.Screen name="ForgotPassword" component={ForgotPassword} />
-    </Stack.Navigator>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
